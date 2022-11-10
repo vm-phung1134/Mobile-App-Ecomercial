01f60e8167640ef04a89d20979fb0e7453605b68
Quang Thuy <qthuy2609@gmail.com>
2022-11-10 19:29:16 +0700

    add carousel slider

--- lib/ui/homepage/components/body.dart
+++ lib/ui/homepage/components/body.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/ui/homepage/components/collection_card.dart';
 import 'package:shop_app/ui/homepage/components/section_title.dart';
 
+import 'package:carousel_slider/carousel_slider.dart';
+
 class Body extends StatelessWidget {
   const Body({
     Key? key,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final List<String> imageSlider = [
+      "assets/images/slide_0.png",
+      "assets/images/slide_1.jpg",
+      "assets/images/slide_2.jpg",
+      "assets/images/slide_3.jpg",
+    ];
     return Padding(
       padding: const EdgeInsets.all(8),
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Image.asset(
-            "assets/images/slide_0.png",
+          CarouselSlider.builder(
+            options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                autoPlayInterval: const Duration(milliseconds: 2000)),
+            itemCount: imageSlider.length,
+            itemBuilder: (context, itemIndex, realIndex) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(imageSlider[itemIndex], fit: BoxFit.cover),
+              );
+            },
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16),
